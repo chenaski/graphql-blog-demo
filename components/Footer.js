466795fcffe9902c6cce25d3b833b13b31b801cf
chenaski <mail2chenaski@gmail.com>
2021-05-20 23:08:03 +0300

    Init

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  const frontendVersion =
+    process.env.NEXT_PUBLIC_FRONTEND_VERSION ?? 'undefined';
+  const backendVersion =
+    process.env.BACKEND_VERSION ?? globalThis?.backendVersion ?? 'undefined';
+
+  return (
+    <footer>
+      Frontend version <b>{frontendVersion}</b> &nbsp; Backend version{' '}
+      <b>{backendVersion}</b>
+      <style jsx>{`
+        footer {
+          padding: 2rem;
+        }
+      `}</style>
+    </footer>
+  );
+};
+
+export default Footer;
